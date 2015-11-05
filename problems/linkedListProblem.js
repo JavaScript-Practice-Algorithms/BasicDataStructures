8acb6f6a957d63b8cfa3f8ec9aea2104ca3e0c4a
Ian Conery <ianconery@gmail.com>
2015-11-04 17:28:39 -0800

    added linked list

---
+++ problems/linkedListProblem.js
@@ -0,0 +1,24 @@
+//Write your implementation of a linked list in JavaScript
+//Your linked list should have a head and a tail, it should contain methods called addToTail, removeHead and contains
+//Each node in your linked list will have a value and a next property
+
+var Node = function(){
+  
+};
+
+var LinkedList = function(){
+
+};
+
+LinkedList.prototype.addToTail = function(){
+
+};
+
+LinkedList.prototype.removeHead = function(){
+
+};
+
+LinkedList.prototype.contains = function(){
+
+};
+
